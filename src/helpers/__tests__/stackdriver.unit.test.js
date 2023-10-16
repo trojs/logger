2f163b35ad9d430cdc66c3cd0a78199ace2a3fad
Pieter Wigboldus <w3n@w3news.org>
2023-10-16 21:52:48 +0200

    Update import for native node modules

--- src/helpers/__tests__/stackdriver.unit.test.js
+++ src/helpers/__tests__/stackdriver.unit.test.js
@@ -1,5 +1,5 @@
 import test from 'node:test'
-import assert from 'assert'
+import assert from 'node:assert'
 import stackdriver from '../stackdriver.js'
 
 test('Test the stackdriver helper', async (t) => {
